13572cef7054446429f039a7bd8bfa1ed395c357
rabernethy <rdaswimmer@gmail.com>
2020-11-12 01:46:56 +0000

    added a few functions. saving work.

--- floatsStuff.c
+++ floatsStuff.c
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <math.h>
 #include <float.h>
+#include <stdlib.h>
 
 #define NORM 0
 #define DNORM 1
@@ -26,10 +27,12 @@
         man = 0.9218750000
         mode = NORM
 */
-
-
-
-
+typedef struct flt {
+    int sign;
+    int exp;
+    float man;
+    int mode;
+}FLT;
 /*
     Write a function get_flt_bits_int to return an integer with the
     bits copied from a float.
@@ -40,90 +43,62 @@
     indirection trick.  This can easily be done in one line of code.
     The function should accept a float and return an int.
 */
+/* Gets the bit representation of a float as an integer. */
+int get_flt_bits_int(float f) {
+    return *(int *)(&f);
+}
 
+/* Gets the first bit and returns 1 if neg and 0 otherwise. */
+char get_flt_sign_char(float f) {
+    return (get_flt_bits_int(f) >> 31 & 1 == 1) ? '1' : '0';
+}
 
+/* Gets the sign bit and returns -1 if neg and 1 otherwise. */
+int get_flt_sign_val(float f) {
+    return (get_flt_bits_int(f) >> 31 & 1 == 1) ? -1 : 1;
+}
 
+/* Gets the exponent part of the float and returns it as a string. */
+char * get_flt_exp_str(float f) {
+    int n = get_flt_bits_int(f) >> 23;
+    char *bits = malloc(sizeof(char) * 8);
+    int i;
+    for(i = 1; i <= 8; i++) {
+        bits[8-i] = (n & 1) + '0';
+        n = n >> 1;
+    } bits[i] = '\0';
+    return bits;
+}
 
-/*
-    Write a function that returns the sign of a float as a char.
-    You should call get_flt_bits_int to get the bits in an int
-    and return '1' if the sign is negative else return '0'.  The
-    function should accept a float and return a string.
-*/
+/* Gets the value of the exponent part. */
+int get_flt_exp_val(float f) {
+    int n = get_flt_bits_int(f) >> 23;
+    int exp = 0;
+    int i;
+    for(i = 0; i < 8; i++) {
+        if(n & 1 == 1)
+            exp += pow(2,i);
+        n = n >> 1;
+    }
+    return exp - BIAS;
+}
 
+/* Gets the mode of the float. */
+int get_flt_exp_mode(float f) {
+    return (get_flt_exp_val(f) == -126) ? DNORM : (get_flt_exp_val(f) == 127) ? SPEC : NORM; 
+}
 
-
-
-/*
-    Write a function that returns the sign of a float as an integer.
-    You should call get_flt_bits_int to get the bits in an int
-    and return -1 if the sign is negative else return 1.  The function
-    should accept a float and return an int.
-*/
-
-
-
-
-/*
-    Write a function to return a string containing the
-    actual binary value of the exponent of a float in a
-    char array.  You should call get_flt_bits_int to get
-    the bits in an int and return the string.
-    Example:
-        for f = -15.375
-            n = 11000001011101100000000000000000
-            the exponent bits are "10000010"
-    The function should accept a float and return a string.
-*/
-
-
-
-
-/*
-    Write a function to return an integer containing the
-    actual integer value of the exponent of a float.  You
-    should call get_flt_bits_int to get the bits in an int
-    and return the int with the exponent value.
-    Example:
-        for f = -15.375
-            n = 11000001011101100000000000000000
-            the exponent bits are 10000010
-            the actual value of the exponent is 3
-    The function should accept a float and return an int.
-*/
-
-
-
-
-/*
-    Write a function to return an integer containing the
-    mode of the exponent of a float.  You should call
-    get_flt_exp_val to get the bits in an int and return
-    the int with the mode value.
-    Example:
-        for f = -15.375
-            n = 11000001011101100000000000000000
-            the exponent bits are 10000010
-            the mode is NORM
-    The function should accept a float and return an int.
-*/
-
-
-
-
-/*
-    Write a function to return a string containing the
-    actual binary value of the mantissa of a float in a
-    char array.  You should call get_flt_bits_int to get
-    the bits in an int and return the string.
-    Example:
-        for f = -15.375
-            n = 11000001011101100000000000000000
-            the mantissa bits are "11101100000000000000000"
-    The function should accept a float and return a string.
-*/
-
-
+/* Gets the mantissa part of the float and returns it as a string. */
+char * get_flt_man_str(float f) {
+    int n = get_flt_bits_int(f);
+    char *bits = malloc(sizeof(char) * 23);
+    int i;
+    for(i = 1; i <= 23; i++) {
+        bits[23-i] = (n & 1) + '0';
+        n = n >> 1;
+    } bits[i] = '\0';
+    return bits;
+}
 
 
 /*
@@ -138,9 +113,18 @@
             the actual value of the mantissa is 0.9218750000
     The function should accept a float and return an int.
 */
-
-
-
+/* Gets the value of the matissa part as a float. */
+float get_flt_man_val(float f) {
+    int n = get_flt_bits_int(f);
+    float man = 0.0;
+    int i;
+    for(i = 23; i > 0; i--) {
+        if(n & 1 == 1)
+            man += pow(2, -i);
+        n = n >> 1;
+    }
+    return man;
+}
 
 /*
     Write a function to return a string containing the
@@ -156,11 +140,35 @@
             and the mantissa bits are "11101100000000000000000"
             The string should be formatted as:
                 "1 10000010 11101100000000000000000" to clearly
+                 1 10000010 1110110000000000000000
+                            11101100000000000000000
                 separate the 3 parts.
     The function should accept a float and return a string.
 */
 
+char * get_flt_bits_str(float f) {
+    
+    char * bits = malloc(sizeof(char) * 35);
+    int index = -1;
+    int i;
 
+    bits[++index] = get_flt_sign_char(f);
+    bits[++index] = ' ';
+
+    char * exp = get_flt_exp_str(f);
+    for(i = 0; i < 8; i++)
+        bits[++index] = exp[i];
+    bits[++index] = ' ';
+    free(exp);
+
+    char * man = get_flt_man_str(f);
+    for(i = 0; i < 23; i++)
+        bits[++index] = man[i];
+    bits[++index] = '\0';
+    free(man);
+
+    return bits;
+}
 
 
 /*
@@ -219,8 +227,15 @@
 */
 int main(){
 
+    float f = -15.375;
 
+    printf("get_flt_sign_char(%f) = %c\n", f, get_flt_sign_char(f));
+    printf("get_flt_sign_val(%f) = %d\n", f, get_flt_sign_val(f));
+    printf("get_flt_exp_str(%f) = %s\n", f, get_flt_exp_str(f));
+    printf("get_flt_exp_val(%f) = %d\n", f, get_flt_exp_val(f));
+    printf("get_flt_exp_mode(%f) = %d\n", f, get_flt_exp_mode(f));
+    printf("get_flt_man_str(%f) = %s\n", f, get_flt_man_str(f));
+    printf("get_flt_man_val(%f) = %f\n", f, get_flt_man_val(f));
+    printf("get_flt_bits_str(%f) = %s\n", f, get_flt_bits_str(f));
 
-
-    return 0;
 }
